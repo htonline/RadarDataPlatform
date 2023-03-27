f641262d65162040778b95aa0567eba8506fbf11
htonline <986344867@qq.com>
2023-03-27 19:39:47 +0800

    用户登录接口, 没有Token

--- src/main/java/com/eladmin/largescreen/service/impl/UserServiceImpl.java
+++ src/main/java/com/eladmin/largescreen/service/impl/UserServiceImpl.java
@@ -8,6 +8,8 @@ import com.eladmin.largescreen.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
@@ -24,7 +26,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("username", userDTO.getUsername());
         queryWrapper.eq("password", userDTO.getPassword());
-        User one = getOne(queryWrapper);
-        return one != null;
+//        如果数据库中有重复的用户名和密码, 则getOne会报错
+//        解决办法: 直接查出一个list, 只要list个数不为0, 就过
+        List<User> list = list(queryWrapper);
+        return list.size() != 0;
+
     }
 }
